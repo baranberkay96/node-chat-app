dcba7bdeb7e1d8a1cbadd85659597aa2b1c99dcc
baranberkay96 <baranberkay96@gmail.com>
2018-12-17 13:04:53 +0300

    broadcasting and emitting diff

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ port = process.env.PORT || 3000;
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+
+    });
+
     socket.on('createMessage', (message) => {
         console.log(message);
         io.emit('newMessage', {
